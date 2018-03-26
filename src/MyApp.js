a608f05329dc83347d2790edb24aca561e180522
Victor Lee <myleevictor@gmail.com>
2018-03-26 15:47:47 +0800

    added Navlinks into myApp

--- src/MyApp.js
+++ src/MyApp.js
@@ -4,14 +4,18 @@ import HomePage from "./HomePage";
 import TwitterFeed from "./TwitterFeed";
 import TacosFeed from "./TacosFeed";
 import TacoDetail from "./TacoDetail";
+import NavLinks from "./NavLinks";
 
 const MyApp = () => {
-  return <div>
+  return (
+    <div>
+      <NavLinks />
       <Route path="/" exact component={HomePage} />
       <Route path="/Twitter" exact component={TwitterFeed} />
       <Route path="/Tacos" exact component={TacosFeed} />
       <Route path="/Tacos/:id" exact component={TacoDetail} />
-    </div>;
+    </div>
+  );
 };
 
 export default MyApp;
